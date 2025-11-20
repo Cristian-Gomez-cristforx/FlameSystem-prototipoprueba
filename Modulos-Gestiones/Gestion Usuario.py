f860e08225af6eb5492c825c55a57ff43463719f
Cristian-Gomez-cristforx <cristforx@gmail.com>
2025-11-20 09:48:42 -0500

    chore: Se completo un poco más de la funcionalidad restablecer contraseña y algunas mejorar visuales y pruebas

--- Modulos-Gestiones/Gestion Usuario.py
+++ Modulos-Gestiones/Gestion Usuario.py
@@ -1,3 +1,4 @@
+import random
 class Usuario:
     usuarios_registrados= []
     def __init__(self,nombre,correo,contraseña,rol,documento,telefono):
@@ -24,9 +25,47 @@ class Usuario:
             if usuari.correo == correo_actual and usuari.contraseña == contra_actual:
                 return usuari
         return None
-    def restablecer_contraseña():
-        print("Hola olvidadizo")
-        pass
+    def restablecer_contraseña(self):
+
+        while True:   
+            verificar_correo=input("\nIngrese la dirección de correo asociada a su cuenta: ")
+
+            for cuenta in Usuario.usuarios_registrados:
+                if cuenta.correo == verificar_correo:
+                    codigo_aleatorio = random.randint(100000,999999)
+                    arroba_index = cuenta.correo.find("@")#la funcion find, me da la poscion del arroba, le paso como parametro, eso que debe buscar
+
+                    nombre_usuario = cuenta.correo[:arroba_index]#usoun slicing,que empieza desde lo normal, hasta el indice que me da la función find, cuando devuelve el arroba.
+
+                    caracteres_a_mostrar = 3 
+
+                    largo_oculto = len(nombre_usuario) - caracteres_a_mostrar#Hago una resta para saber cuales son los carcateres que quedan para tapar con asteriscos
+
+                    parte_oculta = "*" * largo_oculto
+
+                    parte_visible = nombre_usuario[:caracteres_a_mostrar]
+
+                    correo_enmascarado = parte_visible + parte_oculta + cuenta.correo[arroba_index:]
+                    print(f"\nHemos enviado un codigo de 6 digitos a la dirección de correo: {correo_enmascarado}\n")
+                    print(f"--FlameSystem--\n\nEste es tú codigo solicitado para cambio de contraseña: {codigo_aleatorio}")
+                    
+                    ingreso_codigo=int(input("Ingrese el código:  "))
+                    if codigo_aleatorio==ingreso_codigo:
+                        contra_nueva=input("\nIngrese su nueva contraseña: ")
+                        confirmar_contraseña=input("\nConfirme su contraseña, para estar seguro: ")
+                        if contra_nueva==confirmar_contraseña:
+                            cuenta.contraseña=contra_nueva
+                            print("\nSu contraseña ha sido cambiada con éxito")
+                            return True
+                        else:
+                            print("\nLas contraseñas no coinciden, vuelva a ingresar")
+                    else:
+                        print("\nEl código ingresado es incorrecto")
+
+                else:
+                    print("\nLa dirección de correo electronico ingresada, no esta asociada a una cuenta ")
+                
+
 
     
 if __name__=="__main__":
@@ -44,13 +83,17 @@ if __name__=="__main__":
                 admin.iniciar_sesion(actual_correo,actual_contraseña)  
                 usuario_login= admin.iniciar_sesion(actual_correo,actual_contraseña)
                 if usuario_login is None:
-                   print("Inicio de sesión fallido, algunos de los datos no coinciden")
+                   print("\nInicio de sesión fallido, algunos de los datos no coinciden")
                    intentos_de_inicio+=1
                    if intentos_de_inicio>=3:
-                       print("Haz intentado inciar sesión 3 veces y has fallado")
-                       elegir=input("¿Deseas Restablecer la contraseña?:\nsi/no: ")
+                       print("\nHaz intentado inciar sesión 3 veces y has fallado")
+                       elegir=input("¿\nDeseas Restablecer la contraseña?:\nsi/no: ")
                        if elegir.lower()=="si":
                            admin.restablecer_contraseña()
+                           if admin.restablecer_contraseña:
+                               if admin.restablecer_contraseña:
+                                   break
+                           
                        elif elegir.lower()=="no":
                            print("Ok,volviendo al menú incial...")
                            break
